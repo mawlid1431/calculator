08deece193f7401e8554199132896dc834901313
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 14:40:42 +0300

    calcu;ator challenge done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,33 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+ 
+
+
+
+
+function calculator(num1, num2, operator) {
+  let result;
+
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
+  } else {
+    console.log("Invalid operator");
+    return;
+  }
+
+  console.log("Output: " + result);
+}
+
+// Example usages:
+calculator(5, 3, "+"); // Output: 8
+calculator(5, 3, "-"); // Output: 2
+calculator(5, 3, "*"); // Output: 15
+calculator(5, 3, "/"); // Output: 1.6666666666666667
+calculator(5, 3, "%"); // Output: Invalid operator
+
